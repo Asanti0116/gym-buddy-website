16fc36aafeb9b0d4296323cd058f52dbe5aab719
Asanti0116 <108701996+Asanti0116@users.noreply.github.com>
2023-02-05 00:59:06 +0000

    added routes

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-  return "Hello World"
+def home():
+  return render_template('home.html')
+
+
+@app.route("/motivation")
+def motivation():
+  return render_template("motivation.html")
+
+
+@app.route("/workouts")
+def workouts():
+  return render_template("workouts.html")
+
+
+@app.route("/exerciseOfTheDay")
+def exerciseOfTheDay():
+  return render_template("exerciseOfTheDay.html")
+
+
+@app.route("/login")
+def login():
+  return render_template("login.html")
+
+
+@app.route("/notes")
+def notes():
+  return render_template("notes.html")
 
 
 if __name__ == "__main__":
