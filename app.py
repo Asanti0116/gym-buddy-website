debc1ee4f1ace63fd19d95aa1e115f236de62ca5
Asanti0116 <108701996+Asanti0116@users.noreply.github.com>
2023-02-06 00:31:23 +0000

    made a few changes on a few different thing. Mainly focused on making mockups today

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'santiago8675309'
+debug = DebugToolbarExtension(app)
+
 
 @app.route("/")
-def home():
+def home_page():
   return render_template('home.html')
+
+
+@app.route("/exercise-of-the-day")
+def exercise_of_the_day():
+  return render_template('exercise-of-the-day.html')
 
 
 @app.route("/motivation")
@@ -18,9 +27,9 @@ def workouts():
   return render_template("workouts.html")
 
 
-@app.route("/exerciseOfTheDay")
-def exerciseOfTheDay():
-  return render_template("exerciseOfTheDay.html")
+@app.route("/notes")
+def notes():
+  return render_template("notes.html")
 
 
 @app.route("/login")
@@ -28,9 +37,9 @@ def login():
   return render_template("login.html")
 
 
-@app.route("/notes")
-def notes():
-  return render_template("notes.html")
+@app.route("/join-now")
+def join_now():
+  return render_template("join-now.html")
 
 
 if __name__ == "__main__":
